8e93c769e8b7d87ff797fc9374434b4173426fd2
Fredrik Wollsén <fredrik@neam.se>
2016-05-12 14:46:58 +0300

    Also running coverage test groups that do not specify any data profile (could be db-agnostic, or simply should work in all data profiles)

--- _set-codeception-group-args.sh
+++ _set-codeception-group-args.sh
@@ -8,18 +8,22 @@ if [ "$DATA" == "" ]; then
     echo "Warning: DATA is not properly set (Current value is: '$DATA')"
     DATA="not-set"
 fi
+MINIMAL_CODECEPTION_GROUP_ARGS="-g data:$DATA,coverage:minimal -g coverage:minimal"
+BASIC_CODECEPTION_GROUP_ARGS="-g data:$DATA,coverage:basic -g coverage:basic"
+FULL_CODECEPTION_GROUP_ARGS="-g data:$DATA,coverage:full -g coverage:full"
+PARANOID_CODECEPTION_GROUP_ARGS="-g data:$DATA,coverage:paranoid -g coverage:paranoid"
 case "$COVERAGE" in
     "minimal")
-        CODECEPTION_GROUP_ARGS="-g data:$DATA,coverage:minimal"
+        CODECEPTION_GROUP_ARGS="$MINIMAL_CODECEPTION_GROUP_ARGS"
         ;;
     "basic")
-        CODECEPTION_GROUP_ARGS="-g data:$DATA,coverage:minimal -g data:$DATA,coverage:basic"
+        CODECEPTION_GROUP_ARGS="$MINIMAL_CODECEPTION_GROUP_ARGS $BASIC_CODECEPTION_GROUP_ARGS"
         ;;
     "full")
-        CODECEPTION_GROUP_ARGS="-g data:$DATA,coverage:minimal -g data:$DATA,coverage:basic -g data:$DATA,coverage:full"
+        CODECEPTION_GROUP_ARGS="$MINIMAL_CODECEPTION_GROUP_ARGS $BASIC_CODECEPTION_GROUP_ARGS $FULL_CODECEPTION_GROUP_ARGS"
         ;;
     "paranoid")
-        CODECEPTION_GROUP_ARGS="-g data:$DATA,coverage:minimal -g data:$DATA,coverage:basic -g data:$DATA,coverage:full -g data:$DATA,coverage:paranoid"
+        CODECEPTION_GROUP_ARGS="$MINIMAL_CODECEPTION_GROUP_ARGS $BASIC_CODECEPTION_GROUP_ARGS $FULL_CODECEPTION_GROUP_ARGS $PARANOID_CODECEPTION_GROUP_ARGS"
         ;;
     *)
         echo "Error: COVERAGE is not properly set (Current value is: '$COVERAGE')"
